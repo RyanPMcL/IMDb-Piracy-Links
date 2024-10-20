7bba7a701f52121f14d5a7e327f0ed7b5c6c460b
RyanPMcL <admin@ryan-mclaughlin.ca>
2024-10-20 17:51:44 -0400

    update IMDbPL.js header

--- IMDbPL.js
+++ IMDbPL.js
@@ -1,5 +1,7 @@
+// ==UserScript==
 // @name        IMDb: Piracy Links
 // @description A script to easily access piracy related links on IMDb pages.
 // @version     0.1
 // @author      Ryan McLaughlin
 // @namespace   https://github.com/RyanPMcL/IMDb-Piracy-Links
+// ==/UserScript==
