84452eaf99e91bfc68bf7accc9dd05222dd6dbe1
Ryan McLaughlin <github@ryan-mclaughlin.ca>
2025-03-07 11:05:35 -0500

    Version Bump for Merge

--- IMDbPL.user.js
+++ IMDbPL.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        IMDb Piracy Links
 // @description A script to easily access piracy related links on IMDb pages.
-// @version     Alpha-6
+// @version     0.1
 // @author      Ryan McLaughlin
 // @namespace   https://ryan-mclaughlin.ca
 // @updateURL   https://raw.githubusercontent.com/RyanPMcL/IMDb-Piracy-Links/refs/heads/main/IMDbPL.user.js
